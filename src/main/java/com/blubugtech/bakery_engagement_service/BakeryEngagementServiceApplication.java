4c8e17b8bec2fccbc939ee1ba02a1870f21556fa
Aman Kumar Maurya <amankrmj@gmail.com>
2026-07-25 22:16:21 +0530

    fix: add MethodSecurityConfig and KafkaConfig imports to disable default Spring Security form login and enable Kafka dead-letter error handling

--- src/main/java/com/blubugtech/bakery_engagement_service/BakeryEngagementServiceApplication.java
+++ src/main/java/com/blubugtech/bakery_engagement_service/BakeryEngagementServiceApplication.java
@@ -2,8 +2,12 @@ package com.blubugtech.bakery_engagement_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import com.blubugtech.common.security.MethodSecurityConfig;
+import com.blubugtech.common.kafka.KafkaConfig;
 
 @SpringBootApplication
+@Import({MethodSecurityConfig.class, KafkaConfig.class})
 public class BakeryEngagementServiceApplication {
 
     public static void main(String[] args) {
