eb9090b0ed368ce19afa146d9994a6d5fe579599
Aman Kumar Maurya <amankrmj@gmail.com>
2026-08-01 10:24:12 +0530

    chore(config): update main application, configuration, and tests

--- src/test/java/com/blubugtech/bakery_engagement_service/BakeryEngagementServiceApplicationTests.java
+++ src/test/java/com/blubugtech/bakery_engagement_service/BakeryEngagementServiceApplicationTests.java
@@ -2,9 +2,24 @@ package com.blubugtech.bakery_engagement_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import com.blubugtech.bakery_engagement_service.search.repository.FeedbackSearchRepository;
+import com.blubugtech.bakery_engagement_service.search.repository.TestimonialSearchRepository;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BakeryEngagementServiceApplicationTests {
+
+    @MockBean
+    private FeedbackSearchRepository feedbackSearchRepository;
+
+    @MockBean
+    private TestimonialSearchRepository testimonialSearchRepository;
+
+    @MockBean
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Test
     void contextLoads() {
